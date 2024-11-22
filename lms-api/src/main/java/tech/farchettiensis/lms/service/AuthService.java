dec9afbec3d648dc3e394295f73f2d06be7bfb96
farchettiensis <farchettiensis@users.noreply.github.com>
2024-11-22 00:28:11 -0300

    feat: set up basic Spring Security and password encryption
    - Added SecurityConfig with basic configurations for CSRF disabling, session management, and request authorization.
    - Integrated BCryptPasswordEncoder for password encryption.
    - Enabled Spring Security's SecurityFilterChain for request-level security.
    - Prepared the application for future authentication and authorization enhancements.

---
+++ lms-api/src/main/java/tech/farchettiensis/lms/service/AuthService.java
@@ -0,0 +1,24 @@
+package tech.farchettiensis.lms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.farchettiensis.lms.repository.UserRepository;
+
+@Service
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
