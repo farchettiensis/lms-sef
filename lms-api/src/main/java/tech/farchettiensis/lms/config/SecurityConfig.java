6347b83f9f4de4815841e7705805450084f3c5be
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2024-11-25 15:27:32 -0300

    feat (#3): add jwt token; update SecurityConfig; create JwtProperties.
    - logging in generates a jwt token.

--- lms-api/src/main/java/tech/farchettiensis/lms/config/SecurityConfig.java
+++ lms-api/src/main/java/tech/farchettiensis/lms/config/SecurityConfig.java
@@ -16,18 +16,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import tech.farchettiensis.lms.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
