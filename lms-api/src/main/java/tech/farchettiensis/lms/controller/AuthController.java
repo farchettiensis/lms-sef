c6fa6d7586e0b2e54ad21db45eb52e1e69966edf
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2024-11-25 13:17:46 -0300

    fix (#3): fix authenticateUser
    - getPrincipal's return had to be casted to UserDetails

--- lms-api/src/main/java/tech/farchettiensis/lms/controller/AuthController.java
+++ lms-api/src/main/java/tech/farchettiensis/lms/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +42,8 @@ public class AuthController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String jwt = tokenService.generateToken(authentication.getPrincipal());
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwt = tokenService.generateToken(userDetails);
 
         AuthResponseDTO response = new AuthResponseDTO(jwt, "Bearer");
 
