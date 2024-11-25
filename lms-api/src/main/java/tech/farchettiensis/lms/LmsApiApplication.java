2f721e666810a7ab228dfb2460e7ec15b731055a
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2024-11-25 15:20:06 -0300

    feat (#3): add jwt token; update SecurityConfig; create JwtProperties.
    - logging in generates a jwt token.

--- lms-api/src/main/java/tech/farchettiensis/lms/LmsApiApplication.java
+++ lms-api/src/main/java/tech/farchettiensis/lms/LmsApiApplication.java
@@ -2,8 +2,11 @@ package tech.farchettiensis.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tech.farchettiensis.lms.config.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class LmsApiApplication {
 
     public static void main(String[] args) {
