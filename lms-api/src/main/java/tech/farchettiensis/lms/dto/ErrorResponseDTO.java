7906d97038e49b99d02c2851d8d7103ce5c718b2
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2024-11-28 22:29:02 -0300

    Add GlobalExceptionHandler, ErrorResponseDTO, ResourceNotFoundException

---
+++ lms-api/src/main/java/tech/farchettiensis/lms/dto/ErrorResponseDTO.java
@@ -0,0 +1,18 @@
+package tech.farchettiensis.lms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private String message;
+    private LocalDateTime timestamp;
+    private String details;
+}
