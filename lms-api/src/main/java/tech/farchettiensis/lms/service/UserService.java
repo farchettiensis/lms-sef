763cac9865357dbfceb7e1df5dcc068ad5273773
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2024-12-02 14:32:24 -0300

    feat(#4): update UserService interface with other CRUD methods; implment them in UserServiceImpl.

--- lms-api/src/main/java/tech/farchettiensis/lms/service/UserService.java
+++ lms-api/src/main/java/tech/farchettiensis/lms/service/UserService.java
@@ -1,12 +1,24 @@
 package tech.farchettiensis.lms.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import tech.farchettiensis.lms.dto.UserRegistrationDTO;
 import tech.farchettiensis.lms.dto.UserResponseDTO;
-import tech.farchettiensis.lms.model.User;
+
+import java.util.Optional;
 
 public interface UserService {
+
     UserResponseDTO registerUser(UserRegistrationDTO registrationDTO);
 
-    User findByEmail(String email);
+    Optional<UserResponseDTO> updateUser(Long id, UserRegistrationDTO registrationDTO);
+
+    Optional<UserResponseDTO> findByEmail(String email);
+
+    Optional<UserResponseDTO> findById(Long id);
+
+    Page<UserResponseDTO> findAll(Pageable pageable);
+
+    void deleteUser(Long id);
 }
 
